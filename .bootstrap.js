f6b463dea8344698b5d7ba3294a8bf6d433287e7
Vibesdk <vibesdk-bot@cloudflare.com>
2026-02-17 18:37:50 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -81,7 +81,8 @@ function runSetupCommands() {
     "bun add react-markdown@^9.0.1",
     "bun add react-syntax-highlighter@^15.5.0",
     "bun add react-speech-recognition@^3.10.0",
-    "bun add regenerator-runtime"
+    "bun add regenerator-runtime",
+    "bun add react-syntax-highlighter"
 ];
     
     if (commands.length === 0) {
